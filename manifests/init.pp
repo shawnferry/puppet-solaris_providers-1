906f06fc69b8acfca3afda927e4851411dcfcfb5
Shawn Ferry <sferry@gmail.com>
2017-06-22 14:09:04 -0400

    zone examples...in progress

---
+++ manifests/init.pp
@@ -0,0 +1,1 @@
+class solaris_providers ()
---
+++ manifests/zone.pp
@@ -0,0 +1,19 @@
+# Provide addtional tooling for zones to use templates and
+# structured data
+class solaris_providers::zone (
+  # solaris_providers::zone::zones
+  # Array of zones to create in hash form
+  $zones.each |Hash $zone| {
+    zone { $zone['name']:
+      ensure         => $zone['state'],
+      zonepath       => $zone['path'],
+      install_args   => epp('solaris_providers/zone/install_args.epp',
+      {'zone_data'   => $zone}),
+      zonecfg_export => epp('solaris_providers/zone/zonecfg_export.epp',
+      {'fields' => $facts['zonecfg_export_fields'],
+       'zone_data'   => $zone}),
+      sysidcfg       => epp('solaris_providers/zone/sysidcfg.epp'
+      {'zone_data'   => $zone})
+    }
+  }
+)
